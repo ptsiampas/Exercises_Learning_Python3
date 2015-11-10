4909dd4dd8465b57fcd80f17dd5ef9bc54631f8d
Peter Tsiampas <peter@tsiampas.com>
2015-11-10 19:57:40 +1100

    Exercise 14.11.3 - Solution

---
+++ 14_List Algorithms/Exercise_14.11.3.py
@@ -0,0 +1,23 @@
+# Adapt the queens program so that we keep a list of solutions that have already printed, so that we don’t print
+# the same solution more than once.
+from queens_functions import *
+
+
+def main():
+    import random
+    rng = random.Random()  # Instantiate a generator
+    solutions = []
+    bd = list(range(8))  # Generate the initial permutation
+    num_found = 0
+    tries = 0
+    while num_found < 90:
+        rng.shuffle(bd)
+        tries += 1
+        if not has_clashes(bd):
+            if bd not in solutions:
+                print("Found solution {0} in {1} tries.".format(bd, tries))
+                tries = 0
+                solutions.append(bd[:])
+                num_found += 1
+
+main()
