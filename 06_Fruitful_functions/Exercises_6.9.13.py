98924b81609dbb26ec4f5c8de6b51a3541c084f0
Peter Tsiampas <peter@tsiampas.com>
2015-11-12 21:57:32 +1100

    Formating Changes

--- 06_Fruitful_functions/Exercises_6.9.13.py
+++ 06_Fruitful_functions/Exercises_6.9.13.py
@@ -16,17 +16,19 @@
 import sys
 import math
 
+
 def test(actual, expected):
     """ Compare the actual to the expected value,
         and print a suitable message.
     """
-    linenum = sys._getframe(1).f_lineno   # Get the caller's line number.
+    linenum = sys._getframe(1).f_lineno  # Get the caller's line number.
     if (expected == actual):
         msg = "Test on line {0} passed.".format(linenum)
     else:
         msg = ("Test on line {0} failed. Expected '{1}', but got '{2}'."
-                .format(linenum, expected, actual))
+               .format(linenum, expected, actual))
     print(msg)
+
 
 # Online whacky solution, found on redit, written in java script. so I converted it.
 # var ydelta = y2 - y1;
@@ -47,9 +49,9 @@ def slope(x1, y1, x2, y2):
     ydelta = (y1 - y2)
     xdelta = (x1 - x2)
 
-    if xdelta !=0:
-        delta=float(ydelta) / xdelta        # I had originally done the whole thing as a float, but I just needed
-                                            # to only do one side of the equation to convert it to a double.
+    if xdelta != 0:
+        delta = float(ydelta) / xdelta  # I had originally done the whole thing as a float, but I just needed
+        # to only do one side of the equation to convert it to a double.
     else:
         delta = math.tan(89.99999 * math.pi / 180)
         if y2 < y1:
@@ -57,15 +59,16 @@ def slope(x1, y1, x2, y2):
     return delta
 
 
-def intercept(x1, y1, x2, y2):          # google: calculate the x and y intercepts of a line for more invo
+def intercept(x1, y1, x2, y2):  # google: calculate the x and y intercepts of a line for more invo
     """
     Use a call to slope in a new function named intercept(x1, y1, x2, y2) that returns the y-intercept of the
     line through the points (x1, y1) and (x2, y2)
     Equation: y = mx + b
     """
-    m = slope(x1,y1, x2, y2)
+    m = slope(x1, y1, x2, y2)
     result = y2 - m * x2
     return result
+
 
 def test_suite():
     """ Run the suite of tests for code in this module (this file).
@@ -78,7 +81,8 @@ def test_suite():
     test(intercept(6, 1, 1, 6), 7.0)
     test(intercept(4, 6, 12, 8), 5.0)
 
-test_suite()        # Here is the call to run the tests
+
+test_suite()  # Here is the call to run the tests
 
 # print slope(5, 3, 4, 2)
 # print slope(1, 2, 3, 2)
