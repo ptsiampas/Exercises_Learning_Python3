cf91bfaeb8368effbb4bf08479817c359752d136
Peter Tsiampas <peter@tsiampas.com>
2015-11-18 20:00:50 +1100

    Exercises 17.10.3 - Initial

---
+++ 17_pygame/Exercise_17.10.3.py
@@ -0,0 +1,72 @@
+# Search Google for “sprite sheet playing cards”. Create a list [0..51] to represent an encoding
+# of the 52 cards in a deck. Shuffle the cards, slice off the top five as your hand in
+# a poker deal. Display the hand you have been dealt.
+import pygame
+import random
+
+
+class Card_Sprite:
+    def __init__(self, img):
+        self.image = img
+        self.target_pos = (0, 0)
+
+    def update(self):
+        return
+
+    def darw(self, target_surface, position, card_id):
+        self.target_pos = position
+        x = 65 * card_id
+        y = 0
+        size_x = 66
+        size_y = 95
+        target_surface.blit(self.image, self.target_pos, (x, y, size_x, size_y))
+
+    def handle_click(self):
+        return
+
+    def contains_point(self):
+        return
+
+
+def draw_table(cards):
+    pygame.init()
+    bk_colour = (0, 125, 0)  # Colour of the table.
+    surface_sz = (640, 480)
+    surface = pygame.display.set_mode(surface_sz)
+
+    # Card holders starting point
+    card_size = (66, 95)
+    card_start_loc = (50, 50)
+    card_holder_colour = (255, 255, 0)
+
+    # Build the deck
+    full_deck_sheet = pygame.image.load("pic/CardDeck.png")
+    card1 = Card_Sprite(full_deck_sheet)
+    deck_of_cards = []
+
+    while True:
+        ev = pygame.event.poll()
+        if ev.type == pygame.QUIT:
+            break
+        if ev.type == pygame.KEYDOWN:
+            key = ev.dict['key']
+            if key == 27:  # on Escape key
+                break  # leave the game loop
+        # Update Objects
+
+        # Draw the basic board.
+        surface.fill(bk_colour)
+
+        (x_location, y_location) = card_start_loc
+        for x in range(5):
+            surface.fill(card_holder_colour, (x_location, y_location, card_size[0], card_size[1]))
+            x_location += card_size[0] + 15
+
+        card1.darw(surface, (50, 50), 2)
+        # Flip the surface
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+draw_table([5, 10, 23, 51, 30])
