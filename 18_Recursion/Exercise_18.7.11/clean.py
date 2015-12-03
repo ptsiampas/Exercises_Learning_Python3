a2f66697bbeb3c30a08e1532cb41fc522b72c27b
Peter Tsiampas <peter@tsiampas.com>
2015-12-03 16:52:06 +1100

    Litter files and Clean Files complete

--- 18_Recursion/Exercise_18.7.11/clean.py
+++ 18_Recursion/Exercise_18.7.11/clean.py
@@ -1,1 +1,37 @@
 # Now write a program named cleanup.py that removes all these files.
+import os
+
+
+def clean_file(path):
+    # print(path)
+    os.remove(path)
+
+
+def get_dirlist(path):
+    """
+      Return a sorted list of all entries in path.
+      This returns just the names, not the full path to the names.
+    """
+    dirlist = os.listdir(path)
+    dirlist.sort()
+    return dirlist
+
+
+def print_dir(path):
+    """ Print recursive listing of contents of path """
+    dirlist = get_dirlist(path)
+    for f in dirlist:
+        fullname = os.path.join(path, f)  # Turn name into full pathname
+        if os.path.isfile(fullname):
+            clean_file(fullname)
+        if os.path.isdir(fullname):  # If a directory, recurse.
+            print_dir(fullname)
+
+
+def main():
+    print(print_dir("/home/petert/tmp"))
+    return
+
+
+if __name__ == '__main__':
+    main()
--- 18_Recursion/Exercise_18.7.11/litter.py
+++ 18_Recursion/Exercise_18.7.11/litter.py
@@ -1,2 +1,39 @@
 # Write a program named litter.py that creates an empty file named trash.txt in each subdirectory of a directory
 # tree given the root of the tree as an argument (or the current directory as a default).
+import os
+
+
+def create_trash(path):
+    print(path + "/litter.txt")
+    trash = open(path + "/litter.txt", "w")
+    trash.write("..\n")
+    trash.close()
+
+
+def get_dirlist(path):
+    """
+      Return a sorted list of all entries in path.
+      This returns just the names, not the full path to the names.
+    """
+    dirlist = os.listdir(path)
+    dirlist.sort()
+    return dirlist
+
+
+def print_dir(path):
+    """ Print recursive listing of contents of path """
+    dirlist = get_dirlist(path)
+    for f in dirlist:
+        fullname = os.path.join(path, f)  # Turn name into full pathname
+        if os.path.isdir(fullname):  # If a directory, recurse.
+            create_trash(fullname)
+            print_dir(fullname)
+
+
+def main():
+    print(print_dir("/home/petert/tmp"))
+    return
+
+
+if __name__ == '__main__':
+    main()
