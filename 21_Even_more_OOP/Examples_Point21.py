f80854616c15a644e54a3582624427c25ca8e989
Peter Tsiampas <peter@tsiampas.com>
2015-12-05 18:22:32 +1100

    notes

---
+++ 21_Even_more_OOP/Examples_Point21.py
@@ -0,0 +1,20 @@
+class Point:
+    """ Point class represents and manipulates x,y coords. """
+
+    def __init__(self, x=0, y=0):
+        """ Create a new point at the origin """
+        self.x = x
+        self.y = y
+
+    def distance_from_origin(self):
+        """ Compute my distance from the origin """
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def __str__(self):  # All we have done is renamed the method
+        return "({0}, {1})".format(self.x, self.y)
+
+    def halfway(self, target):
+        """ Return the halfway point between myself and the target """
+        mx = (self.x + target.x) / 2
+        my = (self.y + target.y) / 2
+        return Point(mx, my)
